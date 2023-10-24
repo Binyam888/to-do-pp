3f210185218761c64f8383a4135f79af8e911a67
binyamin <binyam0068@gmail.com>
2023-10-24 14:16:57 +0530

    created a if condition and took the value from the input feild and palced it on the dom

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const ul = document.getElementById("unorderedLIst")
 console.log(ul)
-const inputFeild = document.getElementById("input")
+let inputFeild = document.getElementById("input")
 console.log(inputFeild)
 const addBttn = document.getElementById("addButton")
 console.log(addBttn)
+
+
+let onClick =()=>{
+    if(inputFeild.value === ""){
+        alert("please write something")
+    }
+    else{let value = inputFeild.value
+        let item = document.createElement('li')
+        item.innerText = value
+        console.log(item)
+        ul.appendChild(item)
+        // to clear the value in the input feild after typed
+        inputFeild.value=""
+        //to focus on the input 
+        inputFeild.focus()
+    }
+    
+}
+
+addBttn.addEventListener('click',onClick)
